e41bcaaba1fefea7326209d1b1b71df387c271d4
Cody Lawdermilt <lawdermiltcody@gmail.com>
2026-08-19 21:32:56 -0500

    Installed tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,9 @@
-import { useEffect, useState } from "react";
-import { getHealth, type HealthResponse } from "./api/healthApi";
-import { CreateUserForm } from "./components/users/createUserForm";
-import { UserList } from "./components/users/userList";
+import { login } from "./components/login/login";
 
 function App() {
-  const [health, setHealth] = useState<HealthResponse | null>(null);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    getHealth()
-      .then(setHealth)
-      .catch((err: Error) => setError(err.message));
-  }, []);
-
-  if (error) {
-    return <p>Backend error: {error}</p>;
-  }
-
-  if (!health) {
-    return <p>Checking backend...</p>;
-  }
-
   return (
     <main>
-      <h1>Family Finance Dashboard</h1>
-      <CreateUserForm />
-      <UserList />
+      <Login />
     </main>
   );
 }
